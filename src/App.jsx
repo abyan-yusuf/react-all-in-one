0dd3a5b62667a80c513d47f9335b094143acca65
abyan-yusuf <98206561+abyan-yusuf@users.noreply.github.com>
2024-01-08 10:53:59 +0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
-import React from 'react'
+import React from "react";
+import {
+  Route,
+  Routes,
+} from "react-router-dom/dist/umd/react-router-dom.development";
+import Home from "./Pages/Home";
 
 const App = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
+  );
+};
 
-export default App
+export default App;
